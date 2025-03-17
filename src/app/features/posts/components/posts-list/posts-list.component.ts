0eae7ab1ed89792115e007a0ea4b7232f6074a23
Mena-Erian <mena3koko2karas1@gmail.com>
2025-03-18 00:17:51 +0200

    posts design

--- src/app/features/posts/components/posts-list/posts-list.component.ts
+++ src/app/features/posts/components/posts-list/posts-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { PostComponent } from '../post/post.component';
 
 @Component({
   selector: 'app-posts-list',
-  imports: [],
+  imports: [PostComponent],
   templateUrl: './posts-list.component.html',
-  styleUrl: './posts-list.component.scss'
+  styleUrl: './posts-list.component.scss',
 })
 export class PostsListComponent {
-
+  posts: any[] = [1, 2, 3, 4, 5, 6, 4];
 }
--- src/app/features/timeline/components/timeline/timeline.component.ts
+++ src/app/features/timeline/components/timeline/timeline.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-
+import { PostsListComponent } from '../../../posts/components/posts-list/posts-list.component';
 @Component({
   selector: 'app-timeline',
-  imports: [],
+  imports: [PostsListComponent],
   templateUrl: './timeline.component.html',
-  styleUrl: './timeline.component.scss'
+  styleUrl: './timeline.component.scss',
 })
-export class TimelineComponent {
-
-}
+export class TimelineComponent {}
