bc59e58819a031e504ba872043a4cb60d3471eee
Mena-Erian <mena3koko2karas1@gmail.com>
2025-03-23 21:59:08 +0200

    add new comment features but still not staple

--- src/app/features/comments/components/comments-list/comments-list.component.ts
+++ src/app/features/comments/components/comments-list/comments-list.component.ts
@@ -1,27 +1,71 @@
+import { CommentsService } from './../../service/comments.service';
 import {
+  AfterViewInit,
   Component,
   computed,
+  ElementRef,
+  inject,
   input,
+  OnDestroy,
   OnInit,
+  Renderer2,
   signal,
+  ViewChild,
   WritableSignal,
 } from '@angular/core';
 import { IComment } from '../../../posts/model/postes.interface';
 import { CommentComponent } from '../comment/comment.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-comments-list',
-  imports: [CommentComponent],
+  imports: [CommentComponent, FormsModule],
   templateUrl: './comments-list.component.html',
   styleUrl: './comments-list.component.scss',
 })
-export class CommentsListComponent {
+export class CommentsListComponent implements AfterViewInit, OnDestroy {
+  show = input.required<boolean>();
   commentsPost = input.required<IComment[]>();
-  // lastComment: WritableSignal<IComment> = signal(
-  //   this.commentsPost()[this.commentsPost().length - 1]
-  // );
+  postId = input.required<string>();
+  private readonly commentsService = inject(CommentsService);
+  private formSubmitListener!: () => void;
+  loading: boolean = false;
   lastComment = computed(
     () => this.commentsPost()[this.commentsPost().length - 1]
   );
-  show = input.required<boolean>();
+  newComment!: string;
+  constructor(private renderer2: Renderer2) {}
+  @ViewChild('newCmtForm') newCmtForm!: ElementRef<HTMLFormElement>;
+  @ViewChild('newCmtInput') newCmtInput!: ElementRef<HTMLInputElement>;
+  ngAfterViewInit(): void {
+    this.submitEventInit();
+  }
+  submitEventInit() {
+    this.formSubmitListener = this.renderer2.listen(
+      this.newCmtForm.nativeElement,
+      'submit',
+      (e: SubmitEvent) => {
+        // console.log('test', e);
+        e.preventDefault();
+      }
+    );
+  }
+
+  createComment(contentCmt: string, postId: string): void {
+    this.loading = true;
+    this.newCmtInput.nativeElement.value = '';
+
+    this.commentsService.createCmt(contentCmt, postId).subscribe({
+      next: (res) => {
+        this.loading = false;
+        console.log(res);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+  ngOnDestroy(): void {
+    this.formSubmitListener();
+  }
 }
--- src/app/features/comments/service/comments.service.ts
+++ src/app/features/comments/service/comments.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentsService {
-
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+  createCmt(contentCmt: string, postId: string): Observable<any> {
+    return this.httpClient.post<any>(`${environment.baseUrl}/comments`, {
+      content: contentCmt,
+      post: postId,
+    });
+  }
+  getPostCmt(postId: string): Observable<any> {
+    return this.httpClient.get<any>(
+      `${environment.baseUrl}/posts/${postId}/comments`
+    );
+  }
+  updateCmt(contentUpdated: FormData, cmtId: string): Observable<any> {
+    return this.httpClient.put<any>(
+      `${environment.baseUrl}/comments/${cmtId}`,
+      contentUpdated
+    );
+  }
+  deleteCmt(cmtId: string): Observable<any> {
+    return this.httpClient.delete<any>(
+      `${environment.baseUrl}/comments/${cmtId}`
+    );
+  }
 }
