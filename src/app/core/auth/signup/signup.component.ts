4d142e0646182ca4faa388cd7696668e0c93dd15
Mena Erian Farouk <135561286+Mena-Erian@users.noreply.github.com>
2025-05-24 23:56:01 +0300

    form grou[

--- src/app/core/auth/signup/signup.component.ts
+++ src/app/core/auth/signup/signup.component.ts
@@ -8,10 +8,19 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import {
+  RxReactiveFormsModule,
+  RxwebValidators,
+} from '@rxweb/reactive-form-validators'; // <-- #2 import module
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterLink, FormsModule, ReactiveFormsModule],
+  imports: [
+    RouterLink,
+    FormsModule,
+    ReactiveFormsModule,
+    RxReactiveFormsModule,
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
@@ -20,9 +29,6 @@ export class SignupComponent implements OnInit {
   private readonly formBuilder = inject(FormBuilder);
   private readonly router = inject(Router);
   signUpForm: FormGroup = {} as FormGroup;
-
-  // Add your component code here
-
   days: number[] = Array.from({ length: 31 }, (_, i) => i + 1);
   months: string[] = [
     'January',
@@ -42,19 +48,69 @@ export class SignupComponent implements OnInit {
     { length: 100 },
     (_, i) => new Date().getFullYear() - i
   );
+  fName: string = '';
+  lName: string = '';
+  day: string = '';
+  month: string = '';
+  year: string = '';
 
   ngOnInit(): void {
     this.initForm();
+    console.log(this.signUpForm.get('email'));
   }
   initForm(): void {
-    this.signUpForm = this.formBuilder.group({
-      name: [null, [Validators.required]],
-      email: [null, [Validators.required]],
-      password: [null, [Validators.required]],
-      rePassword: [null, [Validators.required]],
-      dateOfBirth: [null, [Validators.required]],
-      gender: [null, [Validators.required]],
-    });
+    this.signUpForm = this.formBuilder.group(
+      {
+        name: [null, [Validators.required]],
+        email: [null, [Validators.required, Validators.email]],
+        password: [
+          null,
+          [
+            Validators.required,
+            RxwebValidators.password({
+              validation: {
+                maxLength: 10,
+                minLength: 5,
+                digit: true,
+                upperCase: true,
+              },
+            }),
+          ],
+        ],
+        rePassword: [null, [Validators.required]],
+        dateOfBirth: [null, [Validators.required]],
+        gender: [null, [Validators.required]],
+      },
+      { updateOn: 'blur' }
+    );
   }
-  // ---------------------------
+  submitSignInForm(): void {
+    // if (this.signUpForm.valid) {
+    this.signUpForm.get('name')?.setValue(this.fName + ' ' + this.lName);
+    this.signUpForm
+      .get('dateOfBirth')
+      ?.setValue(`${this.day}-${this.month}-${this.year}`);
+    // }
+    console.log(this.signUpForm.value);
+  }
+  setDay(Event: Event): void {
+    const el = Event.target as HTMLSelectElement;
+    this.day = el.value;
+    console.log(this.day);
+  }
+  setMonth(Event: Event): void {
+    const el = Event.target as HTMLSelectElement;
+    this.month = el.value;
+    console.log(this.month);
+  }
+  setYear(Event: Event): void {
+    const el = Event.target as HTMLSelectElement;
+    this.year = el.value;
+    console.log(this.year);
+  }
+  setGender(Event: Event): void {
+    const el = Event.target as HTMLInputElement;
+    this.signUpForm.get('gender')?.setValue(el.value);
+    console.log(this.signUpForm.get('gender')?.value);
+  }
 }
